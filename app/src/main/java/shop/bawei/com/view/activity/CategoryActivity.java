ec33286274537b56fadbc3996f89036fec632407
caojun123 <caojun@blinnnk.com>
2017-04-21 20:11:48 +0800

    add CategoryActivity

---
+++ app/src/main/java/shop/bawei/com/view/activity/CategoryActivity.java
@@ -0,0 +1,16 @@
+package shop.bawei.com.view.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by cj on 2017/4/21.
+ */
+
+public class CategoryActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
