428001c35a6d17fdd77caee3ae6320e418b5a870
caojun123 <caojun@blinnnk.com>
2017-04-21 20:27:46 +0800

    1.0 xx bug fixed

--- app/src/main/java/shop/bawei/com/view/activity/CategoryActivity.java
+++ app/src/main/java/shop/bawei/com/view/activity/CategoryActivity.java
@@ -3,6 +3,7 @@ package shop.bawei.com.view.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by cj on 2017/4/21.
@@ -12,5 +13,6 @@ public class CategoryActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("myMessage","1.0 bug fixed");
     }
 }
